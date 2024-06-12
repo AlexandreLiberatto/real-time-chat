9d0f4077e499898c75078869108738f045e38c43
Alexandre Liberato <alexandreliberatto@gmail.com>
2024-06-12 19:38:44 -0300

    Foinalizando projeto

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,24 +1,21 @@
-/* ====login elements==== */
-const login = document.querySelector('.login')
-const loginForm = document.querySelector('.login__form')
-const loginInput = document.querySelector('.login__input')
+// login elements
+const login = document.querySelector(".login")
+const loginForm = login.querySelector(".login__form")
+const loginInput = login.querySelector(".login__input")
 
-/* ====chat elements==== */
-const chat = document.querySelector('.chat')
-const chatForm = document.querySelector('.chat__form')
-const chatInput = document.querySelector('.chat__input')
+// chat elements
+const chat = document.querySelector(".chat")
+const chatForm = chat.querySelector(".chat__form")
+const chatInput = chat.querySelector(".chat__input")
+const chatMessages = chat.querySelector(".chat__messages")
 
 const colors = [
-    "White",
-    "LightGray", 
-    "LightYellow", 
-    "LightCyan", 
-    "LightGoldenRodYellow", 
-    "LightSteelBlue", 
-    "Lavender", 
-    "MistyRose", 
-    "LightPink", 
-    "PaleTurquoise" 
+    "cadetblue",
+    "darkgoldenrod",
+    "cornflowerblue",
+    "darkkhaki",
+    "hotpink",
+    "gold"
 ]
 
 const user = { id: "", name: "", color: "" }
@@ -34,21 +31,53 @@ const createMessageSelfElement = (content) => {
     return div
 }
 
+const createMessageOtherElement = (content, sender, senderColor) => {
+    const div = document.createElement("div")
+    const span = document.createElement("span")
+
+    div.classList.add("message--other")
+
+    span.classList.add("message--sender")
+    span.style.color = senderColor
+
+    div.appendChild(span)
+
+    span.innerHTML = sender
+    div.innerHTML += content
+
+    return div
+}
+
 const getRandomColor = () => {
     const randomIndex = Math.floor(Math.random() * colors.length)
     return colors[randomIndex]
 }
 
-const processMessage = ({ data }) => {
-    const { userId, userName, userColor, content } = JSON.parse(data)
+const scrollScreen = () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
 }
 
+const processMessage = ({ data }) => {
+    const { userId, userName, userColor, content } = JSON.parse(data)
+
+    const message =
+        userId == user.id
+            ? createMessageSelfElement(content)
+            : createMessageOtherElement(content, userName, userColor)
+
+    chatMessages.appendChild(message)
+
+    scrollScreen()
+}
 
 const handleLogin = (event) => {
     event.preventDefault()
 
     user.id = crypto.randomUUID()
-    user.name = loginInput.value 
+    user.name = loginInput.value
     user.color = getRandomColor()
 
     login.style.display = "none"
@@ -56,7 +85,6 @@ const handleLogin = (event) => {
 
     websocket = new WebSocket("ws://localhost:8080")
     websocket.onmessage = processMessage
-
 }
 
 const sendMessage = (event) => {
